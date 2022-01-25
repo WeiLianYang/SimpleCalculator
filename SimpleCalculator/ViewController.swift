235b406773a8785cfb6a0e1dffb03f4aaf84386c
William <yangchen_0808@163.com>
2022-01-25 22:44:42 +0800

    init project

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by WilliamYang on 2022/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
