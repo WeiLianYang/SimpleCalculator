d705ed9e0fb79bcac93fa1339a2ce2ecbb5cebc9
William <yangchen_0808@163.com>
2022-01-26 21:53:36 +0800

    integrate SnapKit

---
+++ SimpleCalculator/FunctionButton.swift
@@ -0,0 +1,31 @@
+//
+//  FunctionButton.swift
+//  SimpleCalculator
+//
+//  Created by WilliamYang on 2022/1/26.
+//  自定义功能按钮
+//
+
+import Foundation
+import UIKit
+
+
+class FunctionButton : UIButton {
+    
+    init() {
+        // 自动布局，frame 设置 zero，等同(0,0,0,0)
+        super.init(frame: CGRect.zero)
+        // 为按钮添加边框
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.gray.cgColor
+        // 设置字体颜色
+        self.setTitleColor(UIColor.gray, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 26)
+        self.setTitleColor(UIColor.black, for: .highlighted)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
